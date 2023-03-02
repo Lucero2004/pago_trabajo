b9409506e72c17319ceea7f6a5e376a557321329
María Lucero Gómez Pico <125698535+Lucero2004@users.noreply.github.com>
2023-03-01 21:15:25 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pago_trabajo
 programa diseñado para determinar la paga de un usuario según las horas laborales y el coste de estas horas
+
+# Analisis
+
+# Diseño
+
+### Diagrama de flujo
+![Diagrama de flujo](diagrama.png"diagrama de flujo")
+
---
+++ pago_trabajo.py
@@ -0,0 +1,12 @@
+#Determinar el valor de m del ingreso por horas trabajadas del usuario
+
+
+#input
+n=int(input("introduce tus horas laboradas"))
+p=int(input("introduce el valor de la hora"))
+
+#procesing
+m=n*p
+
+#output
+print("Tu paga es" + str(m))
